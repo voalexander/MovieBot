8d19ffb98028a692a89062a858bb36badf4f8d4d
voalexander <alexhovo@gmail.com>
2020-01-21 03:13:25 -0600

    can register and stuff

--- mb_commands.py
+++ mb_commands.py
@@ -2,6 +2,7 @@ import discord
 import os.path
 import datetime
 import myExceptions
+from datetime import timedelta
 from discord.ext import commands
 from os import path
 
@@ -12,6 +13,7 @@ class MovieBot(commands.Cog):
         self.movieQueue = []
         self.movieTime = datetime.datetime.now()
         self.selectedMovie = 0
+        self.registeredUsers = []
         self.__getData()
 
     def __getData(self):
@@ -21,20 +23,20 @@ class MovieBot(commands.Cog):
             films = queue.split("(||)")[:-1]
             for data in films:
                 filmData = data.split("(@@)")
-                print(str(filmData))
                 self.movieQueue.append(filmData)
 
             time = file.readline()
-            print(time)
             date = time.split(" ")
-            print(date)
             dateFirst = date[0].split("/")
             dateSecond = date[1].split(":")
-            print(dateFirst)
-            print(dateSecond)
             self.movieTime = datetime.datetime(2020, int(dateFirst[0]), int(dateFirst[1]), int(dateSecond[0]), int(dateSecond[1].rsplit('\n')[0]))
 
             self.selectedMovie = int(file.readline())
+
+            regUsers = file.readline().split("(@@@)")[:-1]
+            self.registeredUsers = regUsers
+
+            file.close()
 
 
     def __saveAll(self):
@@ -46,6 +48,9 @@ class MovieBot(commands.Cog):
         file.write(self.movieTime.strftime("%m/%d %H:%M"))
         file.write("\n")
         file.write(str(self.selectedMovie))
+        file.write("\n")
+        for x in self.registeredUsers:
+            file.write(str(x) + "(@@@)")
         file.close()
         print("Saving done")
 
@@ -136,3 +141,49 @@ class MovieBot(commands.Cog):
             await ctx.send("```css" + self.__filmFormat(movieData) + "```")
         except myExceptions.CannotFindFilm:
             await ctx.send("```Cannot find movie: " + title + "```")
+
+    @commands.command('register',help='Register for notifications')
+    async def register(self, ctx):
+        if str(ctx.author.id) not in self.registeredUsers:
+            await ctx.send(f"```css\n{ctx.author} registered!```")
+            self.registeredUsers.append(ctx.author.id)
+            self.__saveAll()
+        else:
+            await ctx.send(f"```css\n{ctx.author} is already registered```")
+        
+    @commands.command('unregister', help='Unregister for notifications')
+    async def unregister(self, ctx):
+        if str(ctx.author.id) in self.registeredUsers:
+            await ctx.send(f"```css\n{ctx.author} unregistered```")
+            self.registeredUsers.remove(str(ctx.author.id))
+            self.__saveAll()
+        else:
+            await ctx.send("```css\nYou were never registered```")
+    
+    @commands.command('announce', help='Alex only permission')
+    async def announce(self, ctx):
+        if str(ctx.author.id) == "154422225275977728":
+            tt = self.__convert_timedelta(self.movieTime - datetime.datetime.now())
+            toPrint = "```css\n[The movie is now " + tt + " away]\n\n"
+            toPrint += "We are watching\n"
+            toPrint += self.__filmFormat(self.movieQueue[self.selectedMovie])
+            toPrint += "```"
+            for user in self.registeredUsers:
+                toPrint += self.bot.get_user(int(user)).mention + " "
+            await ctx.send(toPrint)
+        else:
+            await ctx.send("```css\nnope```")
+
+    def __convert_timedelta(self, duration):
+        days, seconds = duration.days, duration.seconds
+        hours = days * 24 + seconds // 3600
+        minutes = (seconds % 3600) // 60
+        seconds = (seconds % 60)
+        toPrint = ""
+        if days > 0:
+            toPrint += str(days) + " day(s), " + str(hours) + " hours(s), and " + str(minutes) + " minute(s)"
+        elif hours > 0:
+            toPrint += str(hours) + " hours(s), and " + str(minutes) + " minute(s)"
+        else:
+            toPrint += str(minutes) + " minute(s)"
+        return toPrint
